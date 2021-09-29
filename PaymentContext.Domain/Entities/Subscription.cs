22eb8e074bcccdf40c8a406df8e6edb1dbbea902
Lucas <lucasgunner3@gmail.com>
2021-09-28 22:39:49 -0300

    Criando base de subscription e incluindo referencias

--- PaymentContext.Domain/Entities/Subscription.cs
+++ PaymentContext.Domain/Entities/Subscription.cs
@@ -1,7 +1,19 @@
+using System;
+
 namespace PaymentContext.Domain.Entities
 {
     public class Subscription
     {
         
+        public Subscription(DateTime createDate, DateTime lastUpdateDate, DateTime? expireDate) 
+        {
+            CreateDate = createDate;
+            LastUpdateDate = lastUpdateDate;
+            ExpireDate = expireDate;   
+        }
+
+        public DateTime CreateDate { get; private set; }
+        public DateTime LastUpdateDate { get; private set; }
+        public DateTime? ExpireDate { get; private set; }
     }
 }
